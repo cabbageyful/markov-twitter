f640d94457b7e4f277598783b4653cd0aba710cd
Hackbright Student <info@hackbrightacademy.com>
2016-07-15 17:26:25 -0700

    Added Twitter API to the script, untested.

--- markov.py
+++ markov.py
@@ -63,8 +63,10 @@ def make_text(chains):
     # def smart_truncate(content, length=100, suffix='...'):
     #   return content[:length].rsplit(' ', 1)[0]+suffix
     # in our case suffix can be the twitter hashtag
+    
+        
     final_post = final_text[:129]
-    print final_post + " #hbgraceXV"
+    return final_post + " #hbgraceXV"
     
 
 def tweet(chains):
@@ -72,9 +74,23 @@ def tweet(chains):
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
     
-
+    my_tweet = make_text(chains)
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
+    api = twitter.Api(
+        consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+    print api.VerifyCredentials()
+
+    status = api.PostUpdate(my_tweet)
+
+    print status.text
+
+
+
 filenames = sys.argv[1:]
 
 # Open the files and turn them into one long string
@@ -83,6 +99,5 @@ text = open_and_read_file(filenames)
 # Get a Markov chain
 chains = make_chains(text)
 
-make_text(chains)
 # Your task is to write a new function tweet, that will take chains as input
-# tweet(chains)
+tweet(chains)
